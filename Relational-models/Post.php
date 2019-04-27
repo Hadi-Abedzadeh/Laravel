3f973d09a6b9d87c993f97a244c4d273b80ef703
Hadi Abedzadeh <Hadi-Abedzadeh@users.noreply.github.com>
2019-04-27 16:53:30 +0430

    Create Post.php

---
+++ Relational-models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $guarded = []; // not recommended
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+}
