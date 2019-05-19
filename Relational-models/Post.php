f7df274dfeefba31ffcf15f3af119b28a74f52ac
Hadi Abedzadeh <Hadi-Abedzadeh@users.noreply.github.com>
2019-05-19 10:23:59 +0430

    Update Post.php

--- Relational-models/Post.php
+++ Relational-models/Post.php
@@ -17,5 +17,10 @@ class Post extends Model
     {
         return $this->belongsToMany(Category::class);
     }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
