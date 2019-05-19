bf23108920bde632dde211283e44fc11a4df051b
Hadi Abedzadeh <Hadi-Abedzadeh@users.noreply.github.com>
2019-05-19 10:25:25 +0430

    Create CategoryPost.php

---
+++ Relational-models/CategoryPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryPost extends Model
+{
+    protected $table = 'category_post';
+    public $timestamps = false;
+}
